68f74337d583b8f5b71c828980cf726a815f99e7
rame0 <r@me0.biz>
2021-12-18 21:56:28 +0300

    Changed updatet checker

--- .config/polybar/scripts/check-all-updates.sh
+++ .config/polybar/scripts/check-all-updates.sh
@@ -1,20 +1,46 @@
 #!/bin/sh
 #source https://github.com/x70b1/polybar-scripts
+#
+#
+#if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+#    updates_arch=0
+#fi
+#
+## if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
+#if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+#    updates_aur=0
+#fi
+#
+#updates=$(("$updates_arch" + "$updates_aur"))
+#
+#if [ "$updates" -gt 0 ]; then
+#    echo " $updates"
+#else
+#    echo "0"
+#fi
+#
 
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
+updates_flat=$(echo 'n' | flatpak update 2>/dev/null | tail -n +5 | head -2 | wc -l)
+
+if [ "$updates_flat" -gt 0 ]; then
+    echo "f:$updates_flat"
+else
+    echo ""
 fi
 
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-    updates_aur=0
+updates_apt=$(apt list --upgradable 2> /dev/null | grep -c upgradable);
+
+if [ "$updates_apt" -gt 0 ]; then
+    echo "a:$updates_apt"
+else
+    echo ""
 fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+updates=$(("$updates_flat" + "$updates_apt"))
 
 if [ "$updates" -gt 0 ]; then
-    echo " $updates"
+    echo "t $updates"
 else
     echo "0"
 fi
