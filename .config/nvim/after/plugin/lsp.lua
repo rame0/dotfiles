09c37a9f1f04126d5c309aa5c222aec8f3ee4e60
rame0 <r@me0.biz>
2024-05-15 00:21:30 +0300

    Добавил настройки NeoVIM

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,56 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({buffer = bufnr, remap = false})
+
+	local opts = {buffer = bufnr}
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+end)
+
+-- to learn how to use mason.nvim
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	ensure_installed = {
+		'rust_analyzer',
+		'tsserver',
+		'eslint',
+		'lua_ls',
+	},
+
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({})
+		end,
+	},
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+	sources = {
+		{name = 'nvim_lsp'},
+	},
+	mapping = {
+		['<C-y>'] = cmp.mapping.confirm({select = false}),
+		['<C-p'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+		['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+		['<C-Space>'] = cmp.mapping.complete()
+	},
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+})
---
+++ .config/nvim/lua/rame0/init.lua
@@ -0,0 +1,2 @@
+require("rame0.remap")
+require("rame0.set")
